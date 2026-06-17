f55ef31326e0212bdce6b0aa3087ec9850774df2
Martin Petr <martinpetrnp@gmail.com>
2026-06-17 20:11:56 +0200

    Add project packages

---
+++ packages/frontend/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-pjs",
+});
+
+export const metadata: Metadata = {
+  title: "Lemongrass orchestrator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${plusJakartaSans.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ packages/orchestrator-api/dev.Dockerfile
@@ -0,0 +1,14 @@
+# syntax=docker/dockerfile:1
+FROM node:22-alpine
+
+WORKDIR /app
+
+COPY package.json pnpm-lock.yaml pnpm-workspace.yaml ./
+RUN npm install -g pnpm
+RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile --store-dir /pnpm/store
+
+COPY . .
+
+ENV PORT=3000
+
+CMD ["pnpm", "run", "start:dev"]
